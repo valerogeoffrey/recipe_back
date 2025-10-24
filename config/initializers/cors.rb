901d8ee6b5c05a38193bb45d7c4b19d7094e9537
Geoffrey VALERO <valero.geoffrey@gmail.com>
2025-10-25 00:34:46 +0200

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5122', 'localhost:5122', '127.0.0.1:5122', '127.0.0.1:5122'
+    origins 'https://recipe-front-sigma.vercel.app/','https://recipe-front-sigma.vercel.app','localhost:5122', 'localhost:5122', '127.0.0.1:5122', '127.0.0.1:5122'
 
     resource '*',
       headers: :any,
